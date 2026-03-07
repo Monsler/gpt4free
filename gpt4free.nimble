abbeee48a3a53eb68ffa7a008afc7965a984a4ea
Monsler <galileoru22@gmail.com>
2026-03-07 18:17:11 +0300

    fix version

--- gpt4free.nimble
+++ gpt4free.nimble
@@ -1,6 +1,6 @@
 # Package
 
-version       = "0.1.7"
+version       = "0.2.1"
 author        = "Monsler"
 description   = "gpt4free work-in-progress nim implementation"
 license       = "MIT"
--- tests/test_providers.nim
+++ tests/test_providers.nim
@@ -5,7 +5,7 @@ suite "Provider test":
     test "Provider call":
         let bot = newChatCompletion("stepfun/step-3.5-flash:free", Provider.OpenRouter, @[
             newMessage("system", "you are a helpful ai chat bot. If you don't know the answer to user's question just tell 'I dont know' them"),
-            newMessage("user", "hey, when minecraft movie was released?")
+            newMessage("user", "привет, когда выйдет фильм по майнкрафту?")
         ], true)
 
         let response = waitFor bot.createCompletion()
